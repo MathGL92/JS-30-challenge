f29ea2b18449bea4245a9e6379d89538c80ad4c4
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-12 16:48:09 +1100

    challenge day 13 done

--- 13 - Slide in on Scroll/main.js
+++ 13 - Slide in on Scroll/main.js
@@ -12,3 +12,26 @@ return function() {
   if (callNow) func.apply(context, args);
 };
 }
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+
+
+function checkSlide(event) {
+  sliderImages.forEach(image => {
+    // calculate the pixel level of when we want to slide the image (at half of the height)
+    const slideInAt = (window.scrollY + window.innerHeight) - image.height / 2;
+    // bottom of the image
+    const imageBottom = image.offsetTop + image.height;
+    // we want to make sure we scrolled half the image and the image is half shown
+    const isHalfShown = slideInAt > image.offsetTop;
+    const isNotScrolledPast = window.scrollY < imageBottom;
+    if (isHalfShown && isNotScrolledPast) {
+      image.classList.add('active');
+    } else {
+      image.classList.remove('active');
+    }
+  })
+}
+
+window.addEventListener('scroll', debounce(checkSlide));
