25cbdbec5082e3873c39f47657c0c553838b4b6e
Mathieu Longe <mathieu.longe@orange.fr>
2020-10-26 12:52:47 +1100

    day 23 done

--- 23 - Speech Synthesis/main.js
+++ 23 - Speech Synthesis/main.js
@@ -4,3 +4,47 @@ const voicesDropdown = document.querySelector('[name="voice"]');
 const options = document.querySelectorAll('[type="range"], [name="text"]');
 const speakButton = document.querySelector('#speak');
 const stopButton = document.querySelector('#stop');
+
+msg.text = document.querySelector('[name="text"]').value;
+
+function populateVoices() {
+  voices = this.getVoices();
+  const voiceOptions = voices
+    // .filter(voice => voice.lang.includes('en'))
+    .map(voice => `<option value="${voice.name}">${voice.name} (${voice.lang})</option>`)
+    .join('');
+  voicesDropdown.innerHTML = voiceOptions;
+}
+
+function setVoice() {
+  msg.voice = voices.find(voice => voice.name == this.value)
+  toggle();
+}
+
+// everytime you change the voice, it will restart everything
+
+function toggle(startOver = true) {
+  speechSynthesis.cancel();
+  if (startOver) {
+    speechSynthesis.speak(msg);
+  }
+}
+
+function setOption() {
+  console.log(this.name, this.value);
+  msg[this.name] = this.value;
+  toggle();
+}
+
+// add different voice options in dropdown
+speechSynthesis.addEventListener('voiceschanged', populateVoices);
+
+// select a voice from dropdown
+voicesDropdown.addEventListener('change', setVoice);
+
+
+// everytime we change an option, we run setOption
+options.forEach(option => option.addEventListener('change', setOption));
+
+speakButton.addEventListener('click', toggle);
+stopButton.addEventListener('click', () => toggle(false));
